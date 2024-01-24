fe368042a5c428ad147cd9fa6ce0471d8b1ae31a
saci zakaria <sacideveloppeur@gmail.com>
2024-01-24 09:49:58 +0100

    Scaffold

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class CustomeHomeScreen extends StatelessWidget {
+  const CustomeHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+      ),
+      body: const Center(
+        child: Text("ecommerce  Shams"),
+      ),
+    );
   }
 }
