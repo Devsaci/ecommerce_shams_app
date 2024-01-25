0c3b7ee9d2f888b1600ae41e089371a5cc2a1347
saci zakaria <sacideveloppeur@gmail.com>
2024-01-25 15:47:45 +0100

    body:  const TabBarView(children: [],

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   static const TextStyle optionStyle =
-      TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black);
+      TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black);
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +18,13 @@ class HomeScreen extends StatelessWidget {
           bottom: const TabBar(
             tabs: [
               Tab(child: Text('Men', style: optionStyle)),
-              Tab(child: Text('Men', style: optionStyle)),
-              Tab(child: Text('Men', style: optionStyle)),
+              Tab(child: Text('Women', style: optionStyle)),
+              Tab(child: Text('Shoes', style: optionStyle)),
             ],
           ),
         ),
-        body: const Center(
-          child: Text("ecommerce  Shams", style: optionStyle),
+        body: const TabBarView(
+          children: [],
         ),
       ),
     );
