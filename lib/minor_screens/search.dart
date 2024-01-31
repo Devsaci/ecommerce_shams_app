54f1ee29f29b0cfb6741ea5a22f57f0cdcd688f0
saci zakaria <sacideveloppeur@gmail.com>
2024-01-31 16:59:56 +0100

    elevation: 0,

--- lib/minor_screens/search.dart
+++ lib/minor_screens/search.dart
@@ -8,6 +8,7 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
         title: const CupertinoSearchTextField(),
       ),
