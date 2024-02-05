66cc4aae15ecd2f2957c5969b21ccff341ddc73d
saci zakaria <sacideveloppeur@gmail.com>
2024-02-05 16:12:20 +0100

    itemCount: items.length,

--- lib/main_screens/category.dart
+++ lib/main_screens/category.dart
@@ -45,7 +45,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
       width: size.width * 0.2,
       color: Colors.grey.shade300,
       child: ListView.builder(
-        itemCount: 9,
+        itemCount: items.length,
         itemBuilder: (context, index) {
           return SizedBox(
             height: 100,
