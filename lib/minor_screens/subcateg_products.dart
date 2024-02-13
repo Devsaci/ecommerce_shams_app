b6a23c7d799670753c0365c772ed272035118a7c
saci zakaria <sacideveloppeur@gmail.com>
2024-02-13 15:24:22 +0100

    title: const Text("SubCategProduct"),

--- lib/minor_screens/subcateg_products.dart
+++ lib/minor_screens/subcateg_products.dart
@@ -6,7 +6,9 @@ class SubcategProducts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("SubCategProduct"),
+      ),
     );
   }
 }
