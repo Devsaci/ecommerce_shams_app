b16d49f3b831e7e473cac3e29412872dcdf36d23
saci zakaria <sacideveloppeur@gmail.com>
2024-02-13 15:19:03 +0100

    class SubcategProducts extends StatelessWidget

---
+++ lib/minor_screens/subcateg_products.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SubcategProducts extends StatelessWidget {
+  const SubcategProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
